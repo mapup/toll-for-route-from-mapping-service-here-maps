578575e522910e7b4f96ba1ee3a0a604e8727824
pavelchowdhury03 <pavelc@mapup.ai>
2021-01-15 00:16:51 +0530

    Updating readme,code and requirements.txt

--- python/Here_Maps.py
+++ python/Here_Maps.py
@@ -3,11 +3,11 @@ import json
 import requests
 import os
 import flexpolyline as fp
-import polyline as Poly
+import polyline as poly
 
 '''Fetching Polyline from Here Maps'''
 
-#API key for MapmyIndia
+#API key for Here Maps
 key=os.environ.get('Here_Maps_API_Key')
 
 #Source and Destination Coordinates
@@ -18,15 +18,15 @@ source_latitude='32.7767'
 destination_longitude='-74.0060'
 destination_latitude='40.7128'
 
-#Query MapmyIndia with Key and Source-Destination coordinates
+#Query Here Maps with Key and Source-Destination coordinates
 url='https://router.hereapi.com/v8/routes?transportMode=car&origin={a},{b}&destination={c},{d}&apiKey={e}&return=polyline'.format(a=source_latitude,b=source_longitude,c=destination_latitude,d=destination_longitude,e=key)
 
 #converting the response to json
-response=requests.get(url).json()                               #'''TODO : Check for exceptions in response
+response=requests.get(url).json()                               
    
 #Extracting polyline
-polyline_here=response["routes"][0]["sections"][0]['polyline']  #this polyline is here map polypine , but we need google polyline to get Tollguru API working
-polyline=Poly.encode(fp.decode(polyline_here))                  #we decoded here polyline (flexpolyline) into coordinates and encoded that to google polyline
+flex_polyline_here=response["routes"][0]["sections"][0]['polyline']    #heremaps provide a flexpolyline
+polyline_from_heremap=poly.encode(fp.decode(flex_polyline_here))       # we converted that to encoded(google) polyline
 
 
 
@@ -45,9 +45,9 @@ headers = {
           }
 params = {
             'source': "here",
-            'polyline': polyline ,                      #  this is polyline that we fetched from the mapping service     
-            'vehicleType': '2AxlesAuto',                #'''TODO - Need to provide users a slist of acceptable values for vehicle type'''
-            'departure_time' : "2021-01-05T09:46:08Z"   #'''TODO - Specify time formats'''
+            'polyline': polyline_from_heremap ,         #  this is polyline that we fetched from the mapping service     
+            'vehicleType': '2AxlesAuto',                #'''Visit https://tollguru.com/developers/docs/#vehicle-types to know more options'''
+            'departure_time' : "2021-01-05T09:46:08Z"   #'''Visit https://en.wikipedia.org/wiki/Unix_time to know the time format'''
         }
 
 #Requesting Tollguru with parameters
@@ -57,7 +57,7 @@ response_tollguru= requests.post(Tolls_URL, json=params, headers=headers).json()
 if str(response_tollguru).find('message')==-1:
     print('\n The Rates Are ')
     #extracting rates from Tollguru response if there's no error
-    print(*response_tollguru['summary']['rates'].items(),end="\n\n")
+    print(*response_tollguru['route']['costs'].items(),end="\n\n")
 else:
     raise Exception(response_tollguru['message'])
 
